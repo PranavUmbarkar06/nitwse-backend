c92963400bd37c5002b2688f56fa87994d9ab303
Pranav Umbarkar <pu24csb1a50@student.nitw.ac.in>
2025-06-15 17:44:06 +0530

    Update StockHandle.py

--- StockHandle.py
+++ StockHandle.py
@@ -1,14 +1,11 @@
 from flask import Blueprint, request, jsonify
-from flask_pymongo import PyMongo
 
 stockhandle = Blueprint('stockhandle', __name__)
-mongo = None  # Will be initialized in main app
-
-structure = ['Name', 'Price', 'Owner', 'Supply']
+db = None  # Will be initialized in main app
 
 @stockhandle.route('/get_stocks', methods=['GET'])
 def get_stocks():
-    stocks = list(mongo.db.stockdata.find({}, {"_id": 0}))
+    stocks = list(db.stockdata.find({}, {"_id": 0}))
     result = {stock['Name']: {"Name": stock['Name'], "Price": stock['Price']} for stock in stocks}
     return jsonify(result)
 
@@ -23,14 +20,12 @@ def get_remaining_stocks():
     except ValueError:
         return jsonify({"error": "userID must be an integer"}), 400
 
-    user_data = mongo.db.userdata.find_one({"userID": user_id}, {"_id": 0, "stocksOwned": 1})
+    user_data = db.userdata.find_one({"userID": user_id}, {"_id": 0, "stocksOwned": 1})
     user_stocks = set(user_data.get("stocksOwned", {}).keys()) if user_data else set()
 
-    all_stocks = list(mongo.db.stockdata.find({}, {"_id": 0}))
+    all_stocks = list(db.stockdata.find({}, {"_id": 0}))
     remaining_stocks = [stock for stock in all_stocks if stock['Name'] not in user_stocks]
 
     return jsonify({"remaining_stocks": remaining_stocks})
 
-def set_db(database):
-    global mongo
-    mongo = database
+
